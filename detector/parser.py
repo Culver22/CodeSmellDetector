e1439e570cf4ff6496ac6981252932c4c527b8e8
Ollie <o.culver1@newcastle.ac.uk>
2025-06-04 15:54:39 +0100

    Added function which parses a Python file from a file path or a string and returns the Abstract String Tree

---
+++ detector/parser.py
@@ -0,0 +1,22 @@
+import ast
+from pathlib import Path
+
+
+def parse_code(source_or_path):
+    '''
+    Parse Python code from a file path or directly from a string.
+    :param source_or_path: (str or Path) - Either a path to a Python file or Python code as a string.
+    :return: Parsed AST (Abstract Syntax Tree)
+    '''
+
+    # Check if input is a path to a file
+    if isinstance(source_or_path, (str, Path)) and Path(source_or_path).is_file():
+        with open(source_or_path, 'r', encoding='utf-8') as file:
+            # 'utf-8' allows for source_or_path to be parsed on any OS/System (Encoder is specified)
+            source = file.read()
+    else:
+        # Treat input as code string
+        source = source_or_path
+
+    return ast.parse(source)
+
